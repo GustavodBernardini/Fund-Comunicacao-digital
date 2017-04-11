f23bd08eab529e7eda4d34d2fb3adb4ced7202f5
Gustavo Bernardini <gustavodbernardini@hotmail.com>
2017-04-11 08:20:16 -0300

    Test, first commit, should be deleted later.

---
+++ teste_quebra_de_linha.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 07:47:12 2016
+
+@author: gustavodbernardini
+"""
+
+for\
+ i \
+in \
+range\
+ (\
+ 0\
+ , \
+1)\
+:\
+    print("oi", \
+    "tudo bao?" \
+    )
